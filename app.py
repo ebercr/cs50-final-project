6b92ffa23353e361e4867be6335e6c6dfa13e408
Eber Rabello <ebercr@gmail.com>
2023-10-07 01:19:00 -0300

    Import libraries, implement main route in app.py, create final_project.db, add template folder with layout.html, index.html and add static folder with images, icons, script.js and style.css

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///final_project.db")
+
+@app.route("/")
+def index():
+    """Show index page"""
+
+    return render_template("index.html")
+
